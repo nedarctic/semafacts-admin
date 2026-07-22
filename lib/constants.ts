22d85b2a8834a366160a1d10bdf2f3af28bf33a2
nedarctic <nedarctic@gmail.com>
2026-07-23 00:24:36 +0300

    constants file

---
+++ lib/constants.ts
@@ -0,0 +1,38 @@
+export const ALLOWED_MIME_TYPES = [
+    // Images
+    'image/jpeg',
+    'image/png',
+    'image/webp',
+    'image/gif',
+
+    // Audio
+    'audio/mpeg',
+    'audio/wav',
+    'audio/ogg',
+    'audio/mp4',
+
+    // Video
+    'video/mp4',
+    'video/mpeg',
+    'video/webm',
+    'video/quicktime',
+
+    // Documents
+    'application/pdf',
+
+    // Word
+    'application/msword',
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+
+    // Excel
+    'application/vnd.ms-excel',
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+
+    // PowerPoint
+    'application/vnd.ms-powerpoint',
+    'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+
+    // Text
+    'text/plain',
+    'text/csv',
+];
