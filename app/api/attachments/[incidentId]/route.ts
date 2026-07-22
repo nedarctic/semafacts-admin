c733a7f10cc139dd2377c1666da865c21731dda1
nedarctic <nedarctic@gmail.com>
2026-07-23 00:27:34 +0300

    upload attachments handler

---
+++ app/api/attachments/[incidentId]/route.ts
@@ -0,0 +1,48 @@
+import { getServerSession } from "next-auth";
+import { NextResponse, type NextRequest } from "next/server";
+import { authOptions } from "../../auth/[...nextauth]/route";
+
+export async function POST (req: NextRequest, { params }: {
+    params: Promise<{
+        incidentId: string
+    }>
+}) {
+    try {
+        const session = await getServerSession(authOptions);
+        if (!session) {
+            return NextResponse.redirect(`${process.env.NEXT_PUBLIC_BFF_URL}/admin-login`);
+        }
+        const { accessToken } = session;
+        const { incidentId } = await params;
+
+        const formData = await req.formData();
+        const url = `${process.env.BACKEND_API_URL}/attachments/${incidentId}`;
+        const res = await fetch(url, {
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            },
+            body: formData
+        });
+
+        const data = await res.json();
+
+        if(!res.ok){
+            return NextResponse.json({
+                success: false,
+                error: data.error || data.message || "Backend request error"
+            });
+        }
+
+        return NextResponse.json({
+            success: true,
+            data
+        });
+
+    } catch (error) {
+        return NextResponse.json({
+            success: false,
+            error: error instanceof Error ? error.message : String(error)
+        })
+    }
+}
