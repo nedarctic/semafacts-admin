5b673712a5e20f13ef9e0ef75b5d6af2df081981
nedarctic <nedarctic@gmail.com>
2026-07-23 00:27:16 +0300

    update reporting page route handler

---
+++ app/api/reporting-page/route.tsx
@@ -0,0 +1,46 @@
+import { getServerSession } from "next-auth";
+import { NextResponse, type NextRequest } from "next/server";
+import { authOptions } from "../auth/[...nextauth]/route";
+
+export async function PATCH(req: NextRequest) {
+    try {
+        const session = await getServerSession(authOptions);
+        if (!session) {
+            return NextResponse.redirect(`${process.env.NEXT_PUBLIC_BFF_URL}/admin-login`);
+        }
+
+        const { accessToken, user } = session;
+        const { companyId } = user;
+
+        const body = await req.json();
+        const url = `${process.env.BACKEND_API_URL}/reporting-page/${companyId}`;
+        const res = await fetch(url, {
+            method: "PATCH",
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(body)
+        });
+
+        const data = await res.json();
+
+        if (!res.ok) {
+            return NextResponse.json({
+                success: false,
+                error: data.error || data.message || "Backend error request"
+            }, { status: res.status })
+        }
+
+        return NextResponse.json({
+            success: true,
+            data
+        });
+        
+    } catch (error) {
+        return NextResponse.json({
+            success: false,
+            error: error instanceof Error ? error.message : String(error)
+        }, { status: 500 })
+    }
+}
