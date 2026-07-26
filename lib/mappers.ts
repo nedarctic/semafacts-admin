45d5a853f1dc42644ea0707c4d2157ac4cd60706
nedarctic <nedarctic@gmail.com>
2026-07-26 16:14:30 +0300

    mapper for status labels

---
+++ lib/mappers.ts
@@ -0,0 +1,13 @@
+export const statusLabels: {
+    New: string;
+    InReview: string;
+    Investigation: string;
+    Resolved: string;
+    Closed: string;
+} = {
+    New: "New",
+    InReview: "In Review",
+    Investigation: "Investigation",
+    Resolved: "Resolved",
+    Closed: "Closed"
+};
