58fbdf3b75a54a7721e46272df98fd8a142652a9
nedarctic <nedarctic@gmail.com>
2026-07-26 21:03:55 +0300

    get user data for settings page

--- app/handler/settings/page.tsx
+++ app/handler/settings/page.tsx
@@ -1,6 +1,23 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { BreadCrumb } from "@/components/breadcrumb";
+import { getUser } from "@/lib/helpers/users.helpers";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
 export default async function SettingsPage() {
+
+    const session = await getServerSession(authOptions);
+    if (!session) {
+        redirect("/handler-login")
+    }
+    const { user, accessToken } = session;
+
+    console.log("user id", user.id);
+
+    const { data, success } = await getUser(accessToken, user.id);
+
+    console.log("user data", data);
+
     return (
         <div className="flex flex-col min-h-screen gap-6 p-6">
             <BreadCrumb currentPage="Settings" />
